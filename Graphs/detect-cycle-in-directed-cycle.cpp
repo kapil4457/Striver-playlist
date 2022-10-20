6e4b8de244327f464e4710342142362673e6a51d
Kapil Soni <kapilsoni54768161@gmail.com>
2022-10-20 21:42:44 +0530

    .

---
+++ Graphs/detect-cycle-in-directed-cycle.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+    bool dfsCheck(int node, vector<int> adj[], int vis[], int pathVis[])
+    {
+        vis[node] = 1;
+        pathVis[node] = 1;
+
+        for (auto it : adj[node])
+        {
+            if (!vis[it])
+            {
+                if (dfsCheck(it, adj, vis, pathVis) == true)
+                    return true;
+            }
+
+            else if (pathVis[it])
+            {
+                return true;
+            }
+        }
+
+        pathVis[node] = 0;
+        return false;
+    }
+
+public:
+    bool isCyclic(int V, vector<int> adj[])
+    {
+        int vis[V] = {0};
+        int pathVis[V] = {0};
+
+        for (int i = 0; i < V; i++)
+        {
+            if (!vis[i])
+            {
+                if (dfsCheck(i, adj, vis, pathVis) == true)
+                    return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int V, E;
+        cin >> V >> E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++)
+        {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+
+        Solution obj;
+        cout << obj.isCyclic(V, adj) << "\n";
+    }
+
+    return 0;
+}
