6a2b1d9dccd6622796bc255858ab56a86ddb56e2
Kapil Soni <kapilsoni54768161@gmail.com>
2022-12-10 15:00:43 +0530

    .

---
+++ Graphs/Burning_Tree.cpp
@@ -0,0 +1,149 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int ff(Node* root, int target, int level, int &ans, int consider) {
+        if (root == NULL)
+            return -1;
+            
+        if (consider == 1)
+            ans = max(ans, level);
+        
+        if (root->data == target) {
+            ff(root->left, target, 1, ans, 1);
+            ff(root->right, target, 1, ans, 1);
+            return 1;
+        }
+        
+        int left = ff(root->left, target, level+1, ans, consider);
+        int right = ff(root->right, target, level+1, ans, consider);
+        
+        if (left != -1) {
+            ff(root->right, target, left+1, ans, 1);
+            return left+1;
+        }
+        if (right != -1) {
+            ff(root->left, target, right+1, ans, 1);
+            return right+1;
+        }
+        return -1;
+    }
+  
+    int minTime(Node* root, int target) {
+        int ans = 0;
+        ff(root, target, 0, ans, 0);
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() 
+{
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) 
+    {    
+        string treeString;
+        getline(cin, treeString);
+        // cout<<treeString<<"\n";
+        int target;
+        cin>>target;
+        // cout<<target<<"\n";
+
+        Node *root = buildTree(treeString);
+        Solution obj;
+        cout<<obj.minTime(root, target)<<"\n"; 
+
+        cin.ignore();
+
+    }
+
+
+    return 0;
+}
+
+// } Driver Code Ends
